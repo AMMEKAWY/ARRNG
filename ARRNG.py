0989cbc5d86a7de740b83850ceb8d560fca4f42c
Abdelrahman M. Mekawy <41305353+ldviscglein@users.noreply.github.com>
2023-05-04 19:40:42 +0300

    Update ARRNG.py

--- ARRNG.py
+++ ARRNG.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 e_ion=15.8
@@ -5,28 +6,32 @@ e_exc=11.6
 
 def fn(l):
 
-	return(np.exp(-l/e_ion)*(1-np.exp(-l/e_exc)))
-
+	#return(np.exp(-e_ion/l))*(1-np.exp(-l/e_exc))
+	return(np.exp(-l/e_exc))
 
 #def rand(e):
 	
 
-
-
 	
 def rand(e):
 
-	x=np.random.uniform(0,e)
-	y=np.random.uniform(0,1)	
+	x=np.linspace(10**-12,e,10000)
+	y=fn(x)
 
+	i=np.random.randint(0, e)
+	z=np.random.randint(0, 1)
+	
 	while True:
 		
-		if(fn(x)>y):
+		if(y[i]>z):
 		
-			return(x)
+			return(x[i])
 			break
 			
 		else:
 		
-			x=np.random.uniform(0,e)
+			z=np.random.randint(0, 1)
 			#y=np.random.uniform(0,1)
+			
+z=rand(20)
+print(z)
